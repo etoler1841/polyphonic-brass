aa65c024bf4e6fbef13a474e50bd5c86076edce2
Eric Toler <etoler1841@gmail.com>
2020-02-10 17:48:08 -0600

    Implement hero images and add for main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Image from 'gatsby-image'
 
 /**
  * These are placeholder events. This will ultimately read
@@ -10,17 +13,34 @@ import Events from '../components/events'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Home"
-      lang="en"
-      description="The Polyphonic Brass are a brass quintet based in Pensacola, FL."
-    />
-    {/* Hero image/slider */}
-    <Events events={events} />
-    {/* Blog Posts */}
-  </Layout>
-)
+const IndexPage = () => {
+  const { file } = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "hero-main.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 960) {
+            base64
+            aspectRatio
+            src
+            srcSet
+            sizes
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout fluidHeroData={file.childImageSharp.fluid}>
+      <SEO
+        title="Home"
+        lang="en"
+        description="The Polyphonic Brass are a brass quintet based in Pensacola, FL."
+      />
+      <Events events={events} />
+      {/* Blog Posts */}
+    </Layout>
+  )
+}
 
 export default IndexPage
